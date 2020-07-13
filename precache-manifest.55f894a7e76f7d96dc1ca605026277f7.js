968f166758414d84fdf72edbe94a41ddcfbb101b
mendezjose <42753755+mendezjose@users.noreply.github.com>
2020-07-12 19:45:55 -0500

    first commit

---
+++ precache-manifest.55f894a7e76f7d96dc1ca605026277f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4181988d3b0ab6f6f1b3e9cb4714f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6995c4175d81fe3ee4c",
+    "url": "/static/css/2.620ed6e8.chunk.css"
+  },
+  {
+    "revision": "df127e557d3255ffd205",
+    "url": "/static/css/main.193af334.chunk.css"
+  },
+  {
+    "revision": "b6995c4175d81fe3ee4c",
+    "url": "/static/js/2.eb5db451.chunk.js"
+  },
+  {
+    "revision": "442a381b572a91216684f4f4c0bd60c5",
+    "url": "/static/js/2.eb5db451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df127e557d3255ffd205",
+    "url": "/static/js/main.fcd3024c.chunk.js"
+  },
+  {
+    "revision": "cacbe5bffd09db46acda",
+    "url": "/static/js/runtime-main.cdbd9051.js"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "/static/media/Rubik-Medium.c87313aa.ttf"
+  }
+]);
